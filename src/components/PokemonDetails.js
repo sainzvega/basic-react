32e4ec777415c79deb9870811cf97adfe0ea577d
Humberto A. Sainz <SAINZHA@innovasi.com>
2018-03-08 14:45:38 -0800

    Adjust PokemonDetails component to handle new data arch

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -1,28 +1,40 @@
 import React from "react";
-import { Item, Image } from "semantic-ui-react";
+import { Item } from "semantic-ui-react";
+
+const capitalizeName = name => name.charAt(0).toUpperCase() + name.slice(1);
+const concatTypes = types =>
+  types.reduce(
+    (prev, { type }) => (prev.length ? prev + "/" + type.name : type.name),
+    ""
+  );
 
 const PokemonDetails = ({ currentPokemon = {} }) => {
   const {
     id = null,
     name = "",
+    weight = 0,
+    height = 0,
     sprites = {},
     types = [],
-    nickName = "",
-    isCaught = false
+    inPokedex = false
   } = currentPokemon;
 
   return (
     <Item.Group>
-      <Item>
-        <Item.Image size="small" src={sprites.front_default} />
-        <Item.Content>
-          <Item.Header>{`#${id} ${name}`}</Item.Header>
-          <Item.Meta>Maybe Types here?</Item.Meta>
-          <Item.Description>
-            Some Text Here Is in pokedex here?
-          </Item.Description>
-        </Item.Content>
-      </Item>
+      {id && (
+        <Item>
+          <Item.Image size="small" src={sprites.front_default} />
+          <Item.Content>
+            <Item.Header>{`#${id} ${capitalizeName(name)}`}</Item.Header>
+            <Item.Meta>{concatTypes(types)}</Item.Meta>
+            <Item.Description>
+              <div>Weight: {weight}</div>
+              <div>Height: {height}</div>
+              <div>In Pokedex: {inPokedex ? "yes" : "no"}</div>
+            </Item.Description>
+          </Item.Content>
+        </Item>
+      )}
     </Item.Group>
   );
 };
