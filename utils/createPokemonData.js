d1c0eb0530e2b607188788d48451e9e1a53ddfd8
Humberto Sainz <hsainz@talentize.com>
2018-03-18 12:19:18 -0700

    Removed uneccessary utils folder

--- utils/createPokemonData.js
+++
@@ -1,6 +0,0 @@
-const capitalizeName = name => name.charAt(0).toUpperCase() + name.slice(1);
-const concatTypes = types =>
-  types.reduce(
-    (prev, { type }) => (prev.length ? prev + "/" + type.name : type.name),
-    ""
-  );
